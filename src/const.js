d196e21125793f338acfaf99ef6b79b8b0c8df5a
Andrew <8911102@gmail.com>
2020-08-09 12:28:08 +0500

    constants - TASK_COUNT, TASK_COUNT_PER_STEP was moved in other file

--- src/const.js
+++ src/const.js
@@ -7,3 +7,6 @@ export const DESCRIPTIONS = [
 ];
 
 export const MAX_DAYS_GAP = 7;
+
+export const TASK_COUNT = 22;
+export const TASK_COUNT_PER_STEP = 8;
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import {TASK_COUNT, TASK_COUNT_PER_STEP} from "./const";
 import {createSiteMenuTemplate} from "./view/site-menu.js";
 import {createFilterTemplate} from "./view/filter.js";
 import {createTaskTemplate} from "./view/task.js";
@@ -7,9 +8,6 @@ import {createLoadMoreButtonTemplate} from "./view/load-more-button.js";
 import {generateTask} from "./mock/task.js";
 import {generateFilter} from "./mock/filter";
 import {render} from "./view/util/render.js";
-
-const TASK_COUNT = 22;
-const TASK_COUNT_PER_STEP = 8;
 
 const tasks = new Array(TASK_COUNT).fill().map(generateTask);
 const filters = generateFilter(tasks);
