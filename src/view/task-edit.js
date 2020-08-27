6d6e4b6e47cf50f39420822437f3787ca10645c4
Andrew <8911102@gmail.com>
2020-08-27 12:16:41 +0500

    Object assign method was changed

--- src/view/task-edit.js
+++ src/view/task-edit.js
@@ -147,7 +147,7 @@ export const createTaskEditTemplate = (task = {}) => {
 };
 
 export default class TaskEdit {
-  constructor(task = Object.assign(BLANK_TASK)) {
+  constructor(task = Object.assign({}, BLANK_TASK)) {
     this._task = task;
     this._element = null;
   }
