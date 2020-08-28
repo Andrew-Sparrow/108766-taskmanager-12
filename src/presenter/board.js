c6796120d9bb5860aa8e2f220570656875ff8d9a
Andrew <8911102@gmail.com>
2020-08-28 13:58:13 +0500

    5.1.7 Описываем презентер доски. WIP

---
+++ src/presenter/board.js
@@ -0,0 +1,53 @@
+import BoardView from "../view/board.js";
+import SortView from "../view/sort.js";
+import TaskListView from "../view/task-list.js";
+import NoTaskView from "../view/no-task.js";
+import TaskView from "../view/task.js";
+import TaskEditView from "../view/task-edit.js";
+import LoadMoreButtonView from "../view/load-more-button.js";
+
+import {
+  render,
+  RenderPosition
+} from "../view/util/render.js";
+
+export default class Board {
+  constructor(boardContainer) {
+    this._boardContainer = boardContainer;
+
+    this._boardComponent = new BoardView();
+    this._sortComponent = new SortView();
+    this._taskListComponent = new TaskListView();
+    this._noTaskComponent = new NoTaskView();
+  }
+
+  init(boardTasks) {
+    this._boardTasks = boardTasks.slice();
+  }
+
+  _renderSort() {
+
+  }
+
+  _renderTask() {
+
+  }
+
+  _renderTasks() {
+
+  }
+
+  _renderNoTasks() {
+
+  }
+
+  _renderLoadMoreButton() {
+    // Метод, куда уйдёт логика по отрисовке компонетов задачи,
+    // текущая функция renderTask в main.js
+  }
+
+  _renderBoard() {
+    // Метод для инициализации (начала работы) модуля,
+    // бОльшая часть текущей функции renderBoard в main.js
+  }
+}
