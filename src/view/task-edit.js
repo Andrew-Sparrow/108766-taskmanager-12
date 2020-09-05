54c8574750bb214a6dc238622b5d43fe737cfb6d
Andrew <8911102@gmail.com>
2020-09-05 11:36:32 +0500

    6.2.1 Устанавливаем flatpickr. WIP

--- src/view/task-edit.js
+++ src/view/task-edit.js
@@ -9,6 +9,11 @@ import {
   humanizeTaskDueDate,
 } from "./util/task.js";
 
+import flatpickr from "flatpickr";
+
+import "../../node_modules/flatpickr/dist/flatpickr.min.css";
+
+
 const BLANK_TASK = {
   color: COLORS[0],
   description: ``,
